c11c1287459db94b918e65604ff0dafb7cee20b1
Otávio Lima <otavio-lima-co@gmail.com>
2026-01-12 11:10:14 -0300

    fix(projects): correct layout issue

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -1,4 +1,5 @@
 import Skeleton from "../components/Skeleton.tsx";
+import { IoIosArrowForward } from "react-icons/io";
 
 const Projects = () => {
   return (
@@ -9,21 +10,24 @@ const Projects = () => {
           <div className="mt-64 max-[564px]:mt-32">
             <div>
               <h2 className="text-[#D9D9D9] text-xl">To-do List</h2>
-              <div className="flex items-center justify-center border border-[#404040] h-12 w-96 max-[416px]:w-80 max-[346px]:w-72 rounded-2xl mt-4 bg-[#161616] text-[#BFBDBA] text-xl hover:border-[#f2f2f2] hover:text-[#f2f2f2] transition duration-300 select-none shadow-[-2px_2px_4px_rgba(0,0,0,0.5)]">
-                <a href='https://github.com/iotavio-lima/todo-app' target="_blank" rel="noopener noreferrer">To-do List</a>
-              </div>
+              <a href='https://github.com/iotavio-lima/todo-app' target="_blank" rel="noopener noreferrer" className="flex items-center justify-between border border-[#404040] h-12 w-96 px-6 max-[416px]:w-80 max-[346px]:w-72 rounded-xl mt-4 bg-[#0D0D0D] text-[#BFBDBA] text-xl hover:border-[#f2f2f2] hover:text-[#f2f2f2] transition duration-300 select-none shadow-[-2px_2px_4px_rgba(0,0,0,0.5)]">
+                <span>To-do List</span>
+                <span><IoIosArrowForward /></span>
+              </a>
             </div>
             <div className="mt-4">
               <h2 className="text-[#D9D9D9] text-xl">Agents</h2>
-              <div className="flex items-center justify-center border border-[#404040] h-12 w-96 max-[416px]:w-80 max-[346px]:w-72 rounded-2xl mt-4 bg-[#161616] text-[#BFBDBA] text-xl hover:border-[#f2f2f2] hover:text-[#f2f2f2] transition duration-300 select-none shadow-[-2px_2px_4px_rgba(0,0,0,0.5)]">
-                <a href='https://github.com/iotavio-lima/agents' target="_blank" rel="noopener noreferrer">Agents</a>
-              </div>
+              <a href='https://github.com/iotavio-lima/agents' target="_blank" rel="noopener noreferrer" className="flex items-center justify-between border border-[#404040] h-12 w-96 px-6 max-[416px]:w-80 max-[346px]:w-72 rounded-xl mt-4 bg-[#0D0D0D] text-[#BFBDBA] text-xl hover:border-[#f2f2f2] hover:text-[#f2f2f2] transition duration-300 select-none shadow-[-2px_2px_4px_rgba(0,0,0,0.5)]">
+                <span>Agents</span>
+                <span><IoIosArrowForward /></span>
+              </a>
             </div>
             <div className="mt-4">
               <h2 className="text-[#D9D9D9] text-xl">Vollmed Api</h2>
-              <div className="flex items-center justify-center border border-[#404040] h-12 w-96 max-[416px]:w-80 max-[346px]:w-72 rounded-2xl mt-4 bg-[#161616] text-[#BFBDBA] text-xl hover:border-[#f2f2f2] hover:text-[#f2f2f2] transition duration-300 select-none shadow-[-2px_2px_4px_rgba(0,0,0,0.5)]">
-                <a href='https://github.com/iotavio-lima/vollmed-api' target="_blank" rel="noopener noreferrer">Vollmed Api</a>
-              </div>
+              <a href='https://github.com/iotavio-lima/vollmed-api' target="_blank" rel="noopener noreferrer" className="flex items-center justify-between border border-[#404040] h-12 w-96 px-6 max-[416px]:w-80 max-[346px]:w-72 rounded-xl mt-4 bg-[#0D0D0D] text-[#BFBDBA] text-xl hover:border-[#f2f2f2] hover:text-[#f2f2f2] transition duration-300 select-none shadow-[-2px_2px_4px_rgba(0,0,0,0.5)]">
+                <span>Vollmed Api</span>
+                <span><IoIosArrowForward /></span>
+              </a>
             </div>
           </div>
         </div>
