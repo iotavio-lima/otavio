4ad318f964b03a4adc8914e4341b7a392b7fa98e
Otávio Lima <otavio-lima-co@gmail.com>
2026-02-20 20:26:37 -0300

    fix: change a project from projects

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -8,11 +8,11 @@ const Projects = () => {
       content={
         <div className="h-screen overflow-hidden">
           <div className="mt-64 max-[564px]:mt-32 max-w-96">
-            <div className="px-4">
-              <h2 className="text-[#D9D9D9] text-xl font-semibold">To-do List</h2>
-              <p className="text-sm text-[#D9D9D9] mt-2">Fullstack application built with Java, Spring Boot, React, and TypeScript to manage tasks, allowing users to create, list, edit, and complete them.</p>
-              <a href='https://github.com/iotavio-lima/todo-app' target="_blank" rel="noopener noreferrer" className="flex items-center justify-between border border-[#404040] h-12 w-full px-6 rounded-xl mt-4 bg-[#0D0D0D] text-[#BFBDBA] text-xl hover:border-[#f2f2f2] hover:text-[#f2f2f2] transition duration-300 select-none shadow-[-2px_2px_4px_rgba(0,0,0,0.5)]">
-                <span>To-do List</span>
+            <div className="mt-4 px-4">
+              <h2 className="text-[#D9D9D9] text-xl font-semibold">Crown Investments</h2>
+              <p className="text-sm text-[#D9D9D9] mt-2">Full-stack financial management system with a secure REST API (Spring Boot) and a fully responsive frontend (React + TypeScript + TailwindCSS).</p>
+              <a href='https://github.com/iotavio-lima/crown-investments' target="_blank" rel="noopener noreferrer" className="flex items-center justify-between border border-[#404040] h-12 w-full px-6 rounded-xl mt-4 bg-[#0D0D0D] text-[#BFBDBA] text-xl hover:border-[#f2f2f2] hover:text-[#f2f2f2] transition duration-300 select-none shadow-[-2px_2px_4px_rgba(0,0,0,0.5)]">
+                <span>Crown Investments</span>
                 <span><IoIosArrowForward /></span>
               </a>
             </div>
